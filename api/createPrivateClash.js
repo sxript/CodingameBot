453e42e19d1e665883d5b50c3368abf8f8592fd8
SXRIPT <Filip.Adamovic@studierende.htl-donaustadt.at>
2021-05-30 20:23:57 +0200

    add api methods

---
+++ api/createPrivateClash.js
@@ -0,0 +1,7 @@
+const superagent = require('superagent');
+
+const createPrivateClash = async (settingsJSON) => {
+
+};
+
+module.exports = createPrivateClash;
---
+++ api/startClashByHandle.js
@@ -0,0 +1,7 @@
+const superagent = require('superagent');
+
+const startClashByHandle = async (handle) => {
+
+};
+
+module.exports = startClashByHandle;
